5178c9ff6fd70a390c9b63309214b9e9f3081136
luisaugusto <luis.augusto.u@gmail.com>
2022-01-20 12:35:07 -0500

    ok se poblo la base de datos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+movies = URI.open("http://tmdb.lewagon.com/movie/top_rated").read
+
+movies = JSON.parse(movies)
+movies["results"].each do |movie|
+  if movie["original_language"] == "en"
+    pelicula = Movie.new
+    pelicula.title =           movie["original_title"]
+    pelicula.overview =        movie["overview"]
+    pelicula.poster_url =     movie["poster_path"]
+    pelicula.rating =           movie["vote_average"]
+    pelicula.save
+  end
+end
